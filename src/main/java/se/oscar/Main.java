f3a9a787221e159cc8d0091d5b309a1f501f4ffe
Mojujo <oscar.tornqvist01@gmail.com>
2024-09-16 09:54:22 +0200

    Final commit

--- src/main/java/se/oscar/Main.java
+++ src/main/java/se/oscar/Main.java
@@ -1,7 +1,26 @@
 package se.oscar;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        int h = 0;
+        int m = 0;
+        int s = 0;
+
+        while (true) {
+            s++;
+            if (s==60) {
+                m++;
+                s = 0;
+            }
+            if (m==60) {
+                h++;
+                m = 0;
+            }
+            if (h == 24) {
+                h = 0;
+            }
+            System.out.println(h + ":" + m + ":" + s);
+            Thread.sleep(1000);
+        }
     }
 }
